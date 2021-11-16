73026016b7f524fcb4e8c01e9e848f6bea6a42a6
Julius Lang <julius.c.lang@icloud.com>
2021-11-16 14:45:21 +0100

    User provide offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,7 +3,7 @@ class Offer < ApplicationRecord
 
   validates :user, presence: true
   validates :subject, presence: true
-  validates :level, presence: true
-  validates :price, presence: true
+  validates :level, presence: true, inclusion: { in: %w[beginner intermediate advanced] }
+  validates :price, presence: true, numericality: { only_float: true, greater_than: 0 }
   validates :description, presence: true
 end
